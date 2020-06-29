864199c9e1aefe678f86f0123c2ca63ca2499382
Ricardo Arana Reyes <raranare@everis.com>
2020-06-28 22:28:05 -0500

    doc agregado

---
+++ docs/precache-manifest.fcb487a823afbf2d4c0ccb0146e074a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673ed37789acd515e3a02f32dc0aa6a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4155cce21b965831eaea",
+    "url": "/static/css/main.f86e4afd.chunk.css"
+  },
+  {
+    "revision": "2f10c41aa18ce0c1b360",
+    "url": "/static/js/2.1ff57e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ff57e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4155cce21b965831eaea",
+    "url": "/static/js/main.94b1e8ac.chunk.js"
+  },
+  {
+    "revision": "721f1220074a81654e19",
+    "url": "/static/js/runtime-main.89ec5046.js"
+  }
+]);
